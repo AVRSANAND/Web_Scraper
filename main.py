cc7dc7ba4663957577c327306039d334ca1fa0e3
AVRSANAND <avrsanand2003@gmail.com>
2024-07-08 21:32:56 +0530

    Sending Events to Mail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import selectorlib
-
+import smtplib, ssl
+import os
 
 URL = ("http://programmer100.pythonanywhere.com/tours/")
 
@@ -13,21 +14,40 @@ def scrape(url):
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
-    print("Email is sent")
+
+def send_email(extracted):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "knightkingram007@gmail.com"
+    password = "dladnbtwqagaggkd"
+    receiver = "knightkingram007@gmail.com"
+    message = f"""\
+Subject: New Event Found !!
+
+{extracted}
+    """
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print("Email is sent !!")
 
 def store(extracted):
     with open("data.txt", "a") as file:
         file.write(extracted + "\n")
 
+
 def read(extracted):
     with open("data.txt", "r") as file:
         return file.read()
+
 
 if __name__ == "__main__":
     scraped = scrape(URL)
@@ -37,4 +57,5 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(extracted)
+
